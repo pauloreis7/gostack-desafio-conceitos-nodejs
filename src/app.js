488dce8cb012ac32c0f038bc3290fdf5ee5553fc
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2020-10-27 16:10:46 -0300

    Funcionalidades da api adicionadas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body
+  
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repositorie)
+
+  return response.status(200).json(repositorie)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if(repositorieIndex === -1) return response.status(400).json({ "error": "Repositorie not found :(" })
+
+  const repository = {
+    id,
+    title, 
+    url, 
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  }
+
+  repositories[repositorieIndex] = repository
+  
+  return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if(repositorieIndex >= 0) {
+    repositories.splice(repositorieIndex, 1)
+  } else {
+    return response.status(400).json({ "error": "Repositorie not found :(" })
+  }
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+
+  if(repositorieIndex === -1) return response.status(400).json({ "error": "Repositorie not found :(" })
+
+  repositories[repositorieIndex].likes++
+
+  return response.json(repositories[repositorieIndex])
 });
 
 module.exports = app;
